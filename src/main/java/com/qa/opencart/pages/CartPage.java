31826eb72e08b8e1d70aca1839c5ceefc2b29bab
Sh741 <shhivani741@gmail.com>
2023-02-20 14:24:41 +0530

    updated the code as per naveen review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,11 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	
-	String cart ="macBook";
+	By cart =By.id("macBook");
 	
-	
-	
+	public CartPage() {
+		
+	}
+	public void addToCart() {
+		
+	}
 	
 }
