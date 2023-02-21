7319df9181731782e8893bcb0e9536bf9c354fef
Sh741 <88875175+Sh741@users.noreply.github.com>
2023-02-21 14:39:00 +0530

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,18 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class POC {
+
+	
+	By cart =By.id("POC");
+	
+	public CartPage() {
+		
+	}
+	public void POC() {
+		System.out.println("POC is done! ");
+		
+	}
+	
+}
